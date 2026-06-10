daf766718b2f972bf5fdc92249d81a16cdf2c233
LuisSsos <luis.sosa.dev@gmail.com>
2026-06-09 23:49:00 -0300

    pruebav2

--- config/multer.js
+++ config/multer.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
+
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+
+console.log(process.env.CLOUDINARY_API_KEY);
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
